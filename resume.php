1feef5214f799580e2ba97f5df977976cc5c7212
naveenirving <49906086+naveenirving@users.noreply.github.com>
2019-04-23 17:00:15 +0530

    Add initial cv website files

---
+++ resume.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<title>Naveen personal site</title>
+</head>
+<body>
+<table cellspacing='10 '>
+<tr>
+<td>
+<img src="download.jpg" alt="Naveen profile pictures" width='175px' height='175px'>
+</td>
+<td>
+<h1>Naveen</h1>
+<i>The university professor</i><br>
+<i>Department of <a href="">Computer science</a></i><br>
+<i>Department of <a href="">Information science</a></i><br>
+<i><a href="">Council university<a></i><br>
+<i>itholand,NY 1456</i><br>
+</td>
+</tr>
+</table>
+<hr size="3">
+<h2>Hoobies</h2>
+<ul>
+<li>sports</li>
+<li>Video games</li>
+</ul>
+<hr>
+<h3>Work Experience</h2>
+<table cellspacing='10'>
+<thead>
+<tr>
+<th>Year</th>
+<th>Profession</th>
+</tr>
+</thead>
+<tr>
+<td>2014</td>
+<td>Beginner in basketball</td>
+</tr>
+<tr>
+<td>2015-2017</td>
+<td>Basketball pro</td>
+</tr>
+</table>
+<hr>
+<h3>Skills</h3>
+<table cellspacing="10px">
+<tr>
+<td>HTML</td>
+<td>⭐⭐⭐</td>
+<td>CSS</td>
+<td>⭐⭐</td>
+</tr>
+<tr>
+<td>Javascript</td>
+<td>⭐⭐</td>
+<td>php</td>
+<td>⭐⭐⭐⭐</td>
+</tr>
+
+</table>
+
+
+
+</body>
+</html>
